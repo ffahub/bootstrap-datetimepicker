dd35b53348b3bd1f407eb1abbf4b356fdac424ab
Alex <alexlorber@web.de>
2015-11-19 16:41:35 +0100

    fixed bug again

--- js/bootstrap-datetimepicker.js
+++ js/bootstrap-datetimepicker.js
@@ -758,7 +758,7 @@
         months.slice(0, startMonth + 1).addClass('disabled');
       }
       if (year == endYear) {
-        months.slice(endMonth + 1).addClass('disabled');
+        months.slice(endMonth + 2).addClass('disabled');
       }
 
       html = '';
