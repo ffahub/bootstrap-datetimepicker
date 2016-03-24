110724558a90274b9aa971a711b733edbf3ba6e2
Alex <alexlorber@web.de>
2016-03-24 21:13:11 +0100

    changed format of weekday and added english locale

--- js/locales/bootstrap-datetimepicker.de.js
+++ js/locales/bootstrap-datetimepicker.de.js
@@ -6,7 +6,7 @@
 	$.fn.datetimepicker.dates['de'] = {
 		days: ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag"],
 		daysShort: ["Son", "Mon", "Die", "Mit", "Don", "Fre", "Sam", "Son"],
-		daysMin: ["So", "Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"],
+		daysMin: ["Son", "Mon", "Die", "Mit", "Don", "Fri", "Sam", "Son"],
 		months: ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"],
 		monthsShort: ["Jan", "Feb", "Mär", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dez"],
 		today: "Heute",
---
+++ js/locales/bootstrap-datetimepicker.en.js
@@ -0,0 +1,18 @@
+/**
+ * German translation for bootstrap-datetimepicker
+ * Sam Zurcher <sam@orelias.ch>
+ */
+;(function($){
+  $.fn.datepicker.dates['en'] = {
+      days: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+      daysShort: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+      daysMin: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+      months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+      monthsShort: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+      today: "Today",
+      clear: "Clear",
+      format: "mm/dd/yyyy",
+      titleFormat: "D, yyyy-mm-dd",
+      weekStart: 0
+  };
+}(jQuery));
